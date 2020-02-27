9a5292e849e413b3700e871d37cd1827a263ac99
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-27 12:00:32 -1000

    setting up webview layout and loading google.com

--- app/src/main/java/com/myappcompany/russell/webviews/MainActivity.java
+++ app/src/main/java/com/myappcompany/russell/webviews/MainActivity.java
@@ -2,6 +2,8 @@ package com.myappcompany.russell.webviews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("http://www.google.com");
+
+        //Or just load html
+//        webView.loadData("<html><body><h1>Hello world!</h1><p>This is my cool website</p></body></html>", "text/html", "UTF-8");
+
     }
 }
